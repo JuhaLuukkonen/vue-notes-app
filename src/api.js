ffb50b97b53957b44265292615737a1de1c77fbb
JuhaLuukkonen <luukkonenjuha3@gmail.com>
2020-05-05 15:16:46 +0300

    First upload

---
+++ src/api.js
@@ -0,0 +1,33 @@
+export const loadNotes = () => {
+  const savedItems = JSON.parse(localStorage.getItem("notes"));
+  const notesPromise = savedItems
+    ? Promise.resolve(savedItems)
+    : Promise.resolve([
+        {
+          id: 1,
+          title: "Testi1",
+          content:
+            "Sisältö2",
+          status: "New"
+        },
+        {
+          id: 2,
+          title: "Testi2",
+          content:
+            "Sisältö2",
+          status: "Completed"
+        },
+        {
+          id: 3,
+          title: "Testi3",
+          content:
+            "Sisältö3",
+          status: "Not completed"
+        }
+      ]);
+
+  return notesPromise;
+};
+
+export const saveNotes = notes =>
+  localStorage.setItem("notes", JSON.stringify(notes)) && loadNotes();
